4e9bdf99a2269a316bc3b8ce210457bcf05f348d
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-28 15:22:35 +0100

    update commentaires job2

--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -6,7 +6,8 @@ $var2 = "!";
 $space = " ";
 $br = "<br>";
 
-echo "Hello LaPlateforme !"; //Affichage simple sans variable 
-echo $br;
-echo $var1.$space.$var.$space.$var2; /*Affichage avec des variables*/
+echo "Hello LaPlateforme !"; //Affichage simple sans variable (commentaire sur une ligne)
+echo $br; #On passe a la ligne (commentaire sur une ligne)
+echo $var1.$space.$var.$space.$var2; /*Affichage avec 
+des variables (commentaire sur plusieurs lignes*/ 
 ?>
