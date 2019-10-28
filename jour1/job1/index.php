745d70f5e32766a568cf04c30e710f39a7809747
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-28 11:45:45 +0100

    job1 done

---
+++ jour1/job1/index.php
@@ -0,0 +1,23 @@
+<?php  
+
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+
+echo $str2." ".$str." ".$str3;
+
+$val = 6;
+echo "<br>";
+echo $val;
+echo "<br>";
+$val = 6 + 4;
+echo $val;
+
+$myBool = true;
+echo "<br>";
+echo $myBool;
+$myBool = false;
+echo "<br>";
+echo $myBool;
+
+?>
