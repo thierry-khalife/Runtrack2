f939d19278f315fd582f3009b1e00fd172e76837
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-28 11:58:27 +0100

    job 2 done

---
+++ jour1/job2/index.php
@@ -0,0 +1,12 @@
+<?php  
+
+$var1 = "Hello";
+$var = "LaPlateforme";
+$var2 = "!";
+$space = " ";
+$br = "<br>";
+
+echo "Hello LaPlateforme !"; //Affichage simple sans variable 
+echo $br;
+echo $var1.$space.$var.$space.$var2; //Affichage avec des variables
+?>
