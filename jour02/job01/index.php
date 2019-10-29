a880d551c4ae26a75288a5ea2f02b54491f6355f
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-29 15:51:49 +0100

    ajout commentaires job1 et job2

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -4,12 +4,12 @@ $nbr = 0;
 
 while ( $nbr <= 1337) {
 
-	if ($nbr == 42) {
+	if ($nbr == 42) { //si le nombre est egal a 42 on va le mettre en bold et le souligner
 		 echo "<b><u>".$nbr."</b></u>";
 	     echo "<br />";
 	     $nbr++;
 	}
-	else{
+	else{ //pour les autres nombres on affiche en style normal
 		 echo $nbr;
 	     echo "<br />";
 	     $nbr++;
--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -4,10 +4,10 @@ $nbr = 0;
 
 while ( $nbr <= 1337) {
 
-	if ($nbr == 26 || $nbr == 37 || $nbr == 88 || $nbr == 1111 || $nbr == 3233) {
+	if ($nbr == 26 || $nbr == 37 || $nbr == 88 || $nbr == 1111 || $nbr == 3233) { /*verification des conditions si une de ces conditions est valider on n'affiche pas le nombre, on incremente et on passe a la suite*/
 	     $nbr++;
 	}
-	else{
+	else{ //pour le reste on affiche le nombre et on passe a la ligne
 		 echo $nbr;
 	     echo "<br />";
 	     $nbr++;
