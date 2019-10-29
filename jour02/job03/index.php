b1c3d5b895d366e789ffe7c16f187497676b0e2e
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-29 16:10:42 +0100

    commentaires job 3 et 4

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -3,15 +3,15 @@
 $nbr = 0;
 
 while ( $nbr <= 100) {
-    for ($nbr; $nbr<=20; $nbr++) {
+    for ($nbr; $nbr<=20; $nbr++) { //boucle for qui couvre l'interval entre 0 et 20 (qui peut etre remplacer par un if dans ce cas)
         echo "<i>".$nbr."</i>";
 		echo "<br />";
     }
-    for ($nbr; $nbr<25; $nbr++) {
+    for ($nbr; $nbr<25; $nbr++) {  //boucle for qui couvre l'interval entre 20 et 25 (qui peut etre remplacer par un if dans ce cas)
         echo $nbr;
 		echo "<br />";
     }
-    for ($nbr; $nbr<=50; $nbr++) {
+    for ($nbr; $nbr<=50; $nbr++) { //boucle for qui couvre l'interval entre 25 et 50 (qui peut etre remplacer par un if dans ce cas)
         if ($nbr == 42) {
 			echo "LaPlateforme_";
 			echo "<br />";
@@ -21,7 +21,7 @@ while ( $nbr <= 100) {
 	        echo "<br />";
 		}
     } 
-    echo $nbr;
+    echo $nbr;  // affichage nombres entre 50 et 100
     echo "<br />";
     $nbr++;
 }
