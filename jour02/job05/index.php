d64066e4ea4744f2b764d7338b0af0c8a63b5bc2
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-29 15:17:54 +0100

    commit job5

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -1,44 +1,24 @@
-<?php  
+<?php
 
 $nbr = 0;
+$nbdiv = 2; 
 
+while ($nbr <= 1000) {
+    if ($nbr != 1)
+    {
+        while ($nbr % $nbdiv != 0)
+        {
+            $nbdiv++;
+        }
+        if ($nbr == $nbdiv)
+        { 
+            echo $nbr." <br/>";
+        }
 
-      /*
-      while ( $nbr <= 1000) {$nbdiv=1;
-      if(($nbr>=0))
-      {
-      for($i=3; $i<=$nbr; $i++)
-      {
-      $rest=$nbr%$i;
-      if($rest==0)
-      $nbdiv++;
-      }
-      if($nbdiv == 2)
-      echo "<br>".$nbr." est premier";
-      else
-      echo "<br>".$nbr." n'est pas premier";
-
-      }
-      $nbr++;
-      }
-      */
-  while ($nbr <= 1000) {
-  if ($nbr == 1) {
-  echo $nbr.' n\'est pas un nombre premier<br>';
-  $nbr++;
-  continue;
-  }
-  $a = 0;
-  for ($i=2; $i<$nbr; $i++){
-  
-    if (is_int($nbr/$i)){
-    $a++;
-    echo $nbr.' n\'est pas un nombre premier<br>';
-    break;
+        $nbdiv = 2;
     }
-  }
-  if ($a == 0) echo '<b>'.$nbr.' est un nombre premier</b><br>';
-  $nbr++;
-  }
+
+    $nbr++;
+}
 
 ?>
