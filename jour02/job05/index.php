ec331f88fca4782bd7fdb4ee6fd1981e057a17d1
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-29 16:21:41 +0100

    commentaires job 5

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -3,10 +3,10 @@
 $nbr = 0;
 $nbdiv = 2; 
 
-while ($nbr <= 1000) {
-    if ($nbr != 1)
+while ($nbr <= 1000) { // boucle pour sortir touts les nombres premier jusqu'a 1000
+    if ($nbr != 1) //verification que le nombre est different de 1 pour eviter boucle infini
     {
-        while ($nbr % $nbdiv != 0)
+        while ($nbr % $nbdiv != 0) /*modulo du nbr par nbdiv, tant que le modulo est different de 0 on continue le processus jusqu'a ce qu'on tombe sur un 0 pour le modulo et si ce nombre est egal a notre nombre nbr on peut conclure que ce nbr est un nombre premier */
         {
             $nbdiv++;
         }
