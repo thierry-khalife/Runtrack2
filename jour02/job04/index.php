286ea1c009238ef0152bd4a25f597731b41d8138
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-29 13:35:18 +0100

    update 4 + debut 5

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -3,20 +3,23 @@
 $nbr = 0;
 
 while ( $nbr <= 100) {
-   if ($nbr % 3 == 0)
+   if ($nbr % 3 == 0 && $nbr % 5 != 0)
    {
     echo "Fizz";
     echo "<br />";
+    $nbr++;
    }
-   if ($nbr % 5 == 0)
+   if ($nbr % 5 == 0 && $nbr % 3 != 0)
    {
     echo "Buzz";
     echo "<br />";
+    $nbr++;
    }
    if ($nbr % 3 == 0 && $nbr % 5 == 0)
    {
     echo "FizzBuzz";
     echo "<br />";
+    $nbr++;
    }
 
     echo $nbr;
