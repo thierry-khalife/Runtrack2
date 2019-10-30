2c23be78bf600e41955f0a569115afbd583752db
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-30 17:24:56 +0100

    update job3 avec des while

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -3,16 +3,33 @@
 $str = "I'm sorry Dave I'm afraid I can't do that.";
 $voyelles =  array("a", "e", "i", "o", "u", "y","I");
 $i = 0;
+$v = 0;
 $strlegnth = strlen($str);
 
 
-while ($i < $strlegnth) {
+/*while ($i < $strlegnth) {
 		 foreach ($voyelles as $voy){
 		 	   if($str[$i] == $voy) {
                 echo $str[$i];
             }
 		 }
 		  $i++;
+    }*/
+
+while ( $i < $strlegnth )
+{
+    while ($str[$i] != $voyelles[$v] && $v < 6)
+    {
+        $v++;
     }
+
+    if ($str[$i] == $voyelles[$v])
+    {
+        echo $str[$i];
+    }
+
+    $i++;
+    $v = 0;
+}
     
 ?>
