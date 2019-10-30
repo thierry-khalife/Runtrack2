0d8a92f6f235e7568d02e20cba8c242590d1e5bf
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-30 16:38:20 +0100

    update job4 (isset)

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -1,19 +1,13 @@
 <?php  
 
 $str = "Dans l'espace, personne ne vous entend crier";
-$cpt=0;
 $i =0;
-$strlength = strlen($str);
 
 
-while ( $i < $strlength) {
-	if (empty($str) == false)
-    {
-	   $cpt = $cpt + 1;
-    }
-	$i++;
-    }
-
- echo $cpt;
+while (isset($str[$i])!=false)
+{
+    $i++;
+}
+echo $i;
 
 ?>
