e0bafe50a1f4cf266ac6cafe0939d0521a449584
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-30 20:47:27 +0100

    Job5 without in_array & foreach :D

--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -7,30 +7,36 @@
 <?php  
 
 $str = "On nest pas le meilleur quand on le croit mais quand on le sait";
-$dic = array("consonnes"=>["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t","v", "w", "x", "z"],"voyelles"=>["a", "e", "i", "o", "O", "u","y"],"espaces"=>[" "]);
+$dic = array("consonnes"=>["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t","v", "w", "x", "z"],"voyelles"=>["a", "e", "i", "o", "O", "u","y"]);
 $cpt = array("consonnes" => 0, "voyelles" => 0);
 $i =0;
 $strlength = strlen($str);
-
+$voylen = sizeof($dic["voyelles"]);
+$consolen = sizeof($dic["consonnes"]);
 
 while ( $i < $strlength) {
 
-	if (in_array($str[$i],$dic["espaces"])) {
+	if ($str[$i] == " ") {
 		
 	}
-	elseif(in_array($str[$i],$dic["voyelles"])) //in array permet de savoir si notre str[i] existe dans le tableau des voyelles, si oui il nous retourne true
-     {
-         $cpt["voyelles"] =  $cpt["voyelles"] + 1;
-         
-     }
-     else {
-     	$cpt["consonnes"] =  $cpt["consonnes"] + 1;
-        
-
-     }
-
+	else {
+    for($v = 0; $v < $voylen; $v++)
+    {
+         if ($str[$i] == $dic["voyelles"][$v])
+         	{
+                $cpt["voyelles"] =  $cpt["voyelles"] + 1;
+            }
+    }
+    for($v = 0; $v < $consolen; $v++)
+    {
+         if ($str[$i] == $dic["consonnes"][$v])
+         	{
+                $cpt["consonnes"] =  $cpt["consonnes"] + 1;
+            }
+    }
+}
       $i++;
-  }
+ }
 ?>
 <table>
     <thead>
