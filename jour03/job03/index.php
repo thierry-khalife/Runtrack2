c3cd050aa64c671390048785125482d5bc543a84
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-30 19:35:20 +0100

    replacing my second while with a for

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -3,9 +3,8 @@
 $str = "I'm sorry Dave I'm afraid I can't do that.";
 $voyelles =  array("a", "e", "i", "o", "u", "y","I");
 $i = 0;
-$v = 0;
 $strlegnth = strlen($str);
-
+$voylen = sizeof($voyelles);
 
 /*while ($i < $strlegnth) {
 		 foreach ($voyelles as $voy){
@@ -18,18 +17,14 @@ $strlegnth = strlen($str);
 
 while ( $i < $strlegnth )
 {
-    while ($str[$i] != $voyelles[$v] && $v < 6)
+   for($v = 0; $v < $voylen; $v++)
     {
-        $v++;
+         if ($str[$i] == $voyelles[$v])
+         	{
+                echo $str[$i];
+            }
     }
-
-    if ($str[$i] == $voyelles[$v])
-    {
-        echo $str[$i];
-    }
-
     $i++;
-    $v = 0;
 }
     
 ?>
