c33aa3b009302fbd33ae7e16866c60088fa60317
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-30 10:21:12 +0100

    update job4

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -3,24 +3,26 @@
 $nbr = 0;
 
 while ( $nbr <= 100) {
-   if ($nbr % 3 == 0 && $nbr % 5 != 0) //si le modulo 3 egal a 0 et le modulo 5 different de 0 on affiche fizz 
-   {
-    echo "Fizz";
-    echo "<br />";
-    $nbr++;
-   }
-   elseif ($nbr % 5 == 0 && $nbr % 3 != 0) //si le modulo 5 egal a 0 le modulo 3 different de 0 on affiche buzz 
-   {
-    echo "Buzz";
-    echo "<br />";
-    $nbr++;
-   }
-   elseif ($nbr % 3 == 0 && $nbr % 5 == 0) //si le modulo 3 egal a 0 le modulo 5 egal a 0 on affiche fizzbuzz 
+
+   if ($nbr % 3 == 0 && $nbr % 5 == 0) //si le modulo 3 egal a 0 le modulo 5 egal a 0 on affiche fizzbuzz 
    {
     echo "FizzBuzz";
     echo "<br />";
     $nbr++;
    }
+   elseif ($nbr % 3 == 0) //si le modulo 3 egal a 0 et le modulo 5 different de 0 on affiche fizz 
+   {
+    echo "Fizz";
+    echo "<br />";
+    $nbr++;
+   }
+   elseif ($nbr % 5 == 0) //si le modulo 5 egal a 0 le modulo 3 different de 0 on affiche buzz 
+   {
+    echo "Buzz";
+    echo "<br />";
+    $nbr++;
+   }
+
    else{
     echo $nbr;
     echo "<br />";
