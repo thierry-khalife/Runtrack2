ba5b7e26ec3db5afe731cc0d03665408a686de4c
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-10-30 22:00:56 +0100

    update job7

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -1,19 +1,19 @@
 <?php  
 
 $str = "Certaines choses changent, et d'autres ne changeront jamais.";
+$first = $str[0];
 $i =0;
-$strlength = strlen($str);
+$strlength = strlen($str) - 1 ;
 
 
-while ($i < $strlength) {
- if ($i == ($strlength-1)) {
- 	echo $str[0];
- }
- else{
- 	
- 	echo $str[$i+1];
- 	
- }
+while ($i <= $strlength) {
+if ($i == $strlength) {
+	$str[$strlength] = $first;
+}
+else{
+	$str[$i] = $str[$i+1];
+}
  $i++;
  }
+ echo $str;
 ?>
