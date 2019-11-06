e41e13f1de03c21a6060b34c90f8639e2c1cace6
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-11-06 11:40:37 +0100

    commit job01 jour07

---
+++ jour07/job01/index.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['nbvisites']))
+{
+$_SESSION['nbvisites']= 0;
+}
+elseif (isset($_POST["dc"]) == false){
+	$_SESSION['nbvisites']++;
+}
+
+if (isset($_POST["dc"])){
+	session_destroy();
+	$_SESSION['nbvisites']= 0;
+
+}
+echo $_SESSION['nbvisites'];
+
+?>
+
+<form action="index.php" method="post">
+	  <input name="dc" value="Reset Counter" type="submit" />
+</form>
+
+<button><a href="index.php">Clean POST</a></button>
