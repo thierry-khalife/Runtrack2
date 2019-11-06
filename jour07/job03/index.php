40a69fbbe64da161d032f18c76eb602ac86b990f
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-11-06 14:44:33 +0100

    commit job04 jour07

---
+++ jour07/job03/index.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['prenom']))
+{
+    $_SESSION['prenom']= [];
+}
+if (isset($_POST["dc"]) == false && isset($_POST["prenom"])){
+	array_push($_SESSION['prenom'], $_POST['prenom']);
+}
+
+if (isset($_POST["dc"])){
+	session_destroy();
+	header('Location:index.php');
+}
+
+$taille=sizeof($_SESSION['prenom']);
+$i=0;
+
+while ( $i < $taille) {
+	echo $_SESSION['prenom'][$i];
+	echo "<br>";
+	$i++;
+}
+
+?>
+<form action="index.php" method="post">
+	  <input name="prenom"  type="text" required />
+	  <input name="envois" value="nouveau prenom" type="submit" />
+</form>
+
+<form action="index.php" method="post">
+	  <input name="dc" value="Reset Counter" type="submit" />
+</form>
+
